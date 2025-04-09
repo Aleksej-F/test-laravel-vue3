1a34102105bf37415f074a963cb789654b481727
Alexey <72097093+Aleksej-F@users.noreply.github.com>
2025-04-09 21:22:28 +0300

    удалил старое

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,9 @@ import './bootstrap';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia'
 
-import './src/assets/main.css'
+
 import './app.scss';
+
 import App from './App.vue';
 import router from './src/router'
 
